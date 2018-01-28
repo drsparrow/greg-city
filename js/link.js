da2e5da74e1e9f9607d1e457621839bf13f69252
Ari Brenner <aribrenner@gmail.com>
2018-01-27 20:12:45 -0500

    wip

--- js/link.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  var href = document.location.href;
-  document.querySelectorAll('.side-nav a').forEach(function(a) {
-    if (href.includes(a.href)) {
-      a.className += ' selected';
-    }
-  });
-}
